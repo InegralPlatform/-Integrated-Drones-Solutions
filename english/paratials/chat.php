5dbdce06ff2e3f3fe90921485b1d2535118fa453
abobakr8911 <abubakr8911@gmail.com>
2019-04-22 13:55:22 +0200

    added contact button

---
+++ english/paratials/chat.php
@@ -0,0 +1,11 @@
+
+<div id="body">
+
+<div id="chat-circle" class="btn btn-raised">
+        <div id="chat-overlay"></div>
+		    <a href="https://wa.me/009660554133374‬" target="_blank" style="color:#FFF;"><i class="fa fa-phone fa-lg" aria-hidden="true"></i></a>
+	</div>
+
+
+
+</div>
---
+++ english/paratials/our-blog.php
@@ -0,0 +1,103 @@
+<section id="rt_blog">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mt_heading">
+                    <h2 class="section_heading text-center"><span>Our Blog</span></h2>
+                    <p class="heading_txt text-center"><i>Integrated drones solutions in biological precision farming</i> </p>
+
+                </div>
+            </div>
+            <!--=== Post Item ===-->
+            <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/1.jpg" alt="Post Image" class="w-100"/>
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">28</span><span class="month">Oct</span></div>
+                        <h3>Distribution of liquids</h3>
+                    </figcaption>
+                    <a href="Distribution-of-liquids.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+            <!--=== Post Item ===-->
+            <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/2.jpg" alt="Post Image" class="w-100" />
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">15</span><span class="month">Dec</span></div>
+                        <h3>Distribution of granulate products</h3>
+                    </figcaption>
+                    <a href="Distribution-of-granulate products.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+            <!--=== Post Item ===-->
+            <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/3.jpg" alt="Post Image" class="w-100" />
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">01</span><span class="month">Nov</span></div>
+                        <h3>Distribution of powder</h3>
+                    </figcaption>
+                    <a href="Distribution-of-powder.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+                <!--=== Post Item ===-->
+                <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/4.jpg" alt="Post Image" class="w-100" />
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">28</span><span class="month">Oct</span></div>
+                        <h3>Distribution of capsules</h3>
+                    </figcaption>
+                    <a href="Distribution-of-capsules.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+            <!--=== Post Item ===-->
+            <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/5.jpg" alt="Post Image" class="w-100"/>
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">15</span><span class="month">Dec</span></div>
+                        <h3>Surveys</h3>
+                    </figcaption>
+                    <a href="Surveys.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+            <!--=== Post Item ===-->
+            <div class="col-md-4 col-sm-12 grid-item">
+                <figure class="blog_post">
+                    <div class="image">
+                        <img src="images/screenshot/6.jpg" alt="Post Image" class="w-100"/>
+                    </div>
+                    <figcaption>
+                        <div class="date"><span class="day">01</span><span class="month">Nov</span></div>
+                        <h3>Mapping</h3>
+                    </figcaption>
+                    <a href="Mapping.php"></a>
+                </figure>
+            </div>
+            <!--=== End Post Item ===-->
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a href="blog.php" class="rt_btn_color rt_view_btn">View All Posts</a>
+            </div>
+        </div>
+    </div>
+</section>
